959225daf555931b60bc2431f5a38d44d92cd6cb
mukhriddin-m <mukhriddinmeliev@gmail.com>
2022-09-23 16:53:25 +0500

    loader

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MantineUi from './mantine/MantineUi'
+
+function App() {
+  return (
+    <div className="App">
+      <MantineUi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mantine/MantineUi.jsx
@@ -0,0 +1,15 @@
+import { Group, Loader, Text } from '@mantine/core'
+function MantineUi() {
+    return (
+        <Group style={{ display: 'flex', flexDirection: 'column' }} spacing={60}>
+            <Loader color="#42a5f5" size={100} variant="oval" />
+            <Loader color="#4f80e2" size={100} variant="bars" />
+            <Loader color="#4fe0b6" size={100} variant="dots" />
+
+            <Text color='#fff' size={50}>made by alex</Text>
+        </Group>
+
+    );
+}
+
+export default MantineUi;
